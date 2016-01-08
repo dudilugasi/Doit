6887e491e083cc876262704d2e43c8b4fb832303
benmarder <benmarder@live.com>
2016-01-08 17:16:21 +0200

    log in activity 1.0

---
+++ app/src/main/java/com/example/dudilugasi/doit/LogInActivity.java
@@ -0,0 +1,41 @@
+package com.example.dudilugasi.doit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LogInActivity extends AppCompatActivity {
+    private TextView nameText;
+    private TextView passwordText;
+    private TextView phoneText;
+    private Button signUpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        nameText = (TextView) findViewById(R.id.user_name_field);
+        passwordText = (TextView) findViewById(R.id.password_field);
+        phoneText = (TextView) findViewById(R.id.phone_field);
+        signUpButton = (Button)findViewById(R.id.sign_up_button);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //*********call for authentication**********
+             //   Intent intent = new Intent(LogInActivity.this,***next activity***)
+             //   String text1 = nameText.getText().toString();
+             //   intent.putExtra("name",text1);
+             //   String text2 = passwordText.getText().toString();
+             //   intent.putExtra("pass",text2);
+             //   String text3 = phoneText.getText().toString();
+             //   intent.putExtra("phone", text3);
+             //   startActivity(intent);
+            }
+        });
+    }
+
+}
