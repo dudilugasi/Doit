175963180f33fedddc33eaac4e05c330dadb14aa
benmarder <benmarder@live.com>
2016-01-10 11:30:16 +0200

    controller pixed

--- app/src/main/java/com/example/dudilugasi/doit/bl/ITaskController.java
+++ app/src/main/java/com/example/dudilugasi/doit/bl/ITaskController.java
@@ -1,9 +1,12 @@
-package com.example.dudilugasi.doit;
+package com.example.dudilugasi.doit.bl;
+
+import com.example.dudilugasi.doit.common.TaskItem;
+import java.util.List;
 
 public interface ITaskController {
     void SetTask(String task);
     List<TaskItem> GetTasks();
     void  SetTasks(List<TaskItem> tasks);
-    TaskItem GetTaskById(string id);
+    TaskItem GetTaskById(String id);
     void  SetTask(TaskItem task);
 }
